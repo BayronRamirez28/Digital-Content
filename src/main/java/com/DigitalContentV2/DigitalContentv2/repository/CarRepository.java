7ff8a905266d72aabcfaa7632012c7cccf609e3f
Brayan Salguero <107779782+DonBrayaan@users.noreply.github.com>
2023-02-11 20:30:03 -0500

    Update Repository
    Actualización del Repositorio del Carrito

--- src/main/java/com/DigitalContentV2/DigitalContentv2/repository/CarRepository.java
+++ src/main/java/com/DigitalContentV2/DigitalContentv2/repository/CarRepository.java
@@ -3,6 +3,7 @@ package com.DigitalContentV2.DigitalContentv2.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -11,13 +12,19 @@ import com.DigitalContentV2.DigitalContentv2.modelo.Producto;
 import com.DigitalContentV2.DigitalContentv2.modelo.Usuario;
 
 @Repository
-public interface CarRepository extends JpaRepository<Car_items, Integer>{
+public interface CarRepository extends JpaRepository<Car_items, Integer> {
 
 	public List<Car_items> findByCliente(Usuario usuario);
+
+	@Query(value = "select * from cart_items where cliente_id = ? and producto_id = ?", nativeQuery = true)
+	Car_items findByUsuarioAndProducto(Usuario cliente, Producto producto);
+
+	@Modifying
+	@Query(value = "UPDATE cart_items c SET c.cantidad = ?1 WHERE c.producto_id = ?2 AND  c.cliente_id = ?3", nativeQuery = true)
+	public void updateCantidad(Integer cantidad, Integer idProducto, Integer idUsuario);
 	
-	@Query(
-			value = "select * from cart_items where cliente_id = ? and producto_id = ?",
-			nativeQuery = true
-	)Car_items findByUsuarioAndProducto(Usuario cliente, Producto producto);
-	
+	@Modifying
+	@Query(value = "DELETE FROM cart_items WHERE cliente_id = ?1 AND producto_id = ?2", nativeQuery = true)
+	public void deleteByClienteAndProducto(Integer idUsuario, Integer idProducto);	
+
 }
