95c305128bdc41b681ff5742d9dd6a652f96e742
Brayan Salguero <107779782+DonBrayaan@users.noreply.github.com>
2023-02-11 19:16:55 -0500

    Controladores
    Update CarRestController, CarController.

--- src/main/java/com/DigitalContentV2/DigitalContentv2/controller/CarRestController.java
+++ src/main/java/com/DigitalContentV2/DigitalContentv2/controller/CarRestController.java
@@ -3,9 +3,6 @@ package com.DigitalContentV2.DigitalContentv2.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +15,48 @@ public class CarRestController {
 
 	@Autowired
 	private Cardao cardao;
- 	
+
 	@PostMapping("/carrito/add/{pid}/{cant}")
-	public String addProduc2Car(@PathVariable("pid") Integer idProducto,
-			@PathVariable("cant") Integer cant,
-			@AuthenticationPrincipal Authentication authentication, HttpSession session) {
-		
-		if(authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+	public String addProduc2Car(@PathVariable("pid") Integer idProducto, @PathVariable("cant") Integer cant,
+			HttpSession session) {
+
+		System.out.println("addProductCart: " + idProducto + " - " + cant);
+
+		Usuario cliente = (Usuario) session.getAttribute("usersession");
+
+		if (cliente == null) {
 			return "Necesitas Iniciar Sesion Primero";
 		}
-		Usuario cliente = (Usuario) session.getAttribute("usersession"); 
+
 		Integer agregarCantidad = cardao.addProduct(idProducto, cant, cliente);
+		System.out.println("Agregado :D");
 		return agregarCantidad + "item(s) fue agregado a tu carrito.";
+	}
+
+	@PostMapping("/carrito/update/{pid}/{cant}")
+	public String updateCantidad(@PathVariable("pid") Integer idProducto, @PathVariable("cant") Integer cant,
+			HttpSession session) {
+
+		Usuario cliente = (Usuario) session.getAttribute("usersession");
+
+		if (cliente == null) {
+			return "Necesitas Iniciar Sesion Primero";
+		}
+
+		float subtotal = cardao.actualizarCantidad(idProducto, cant, cliente);
+		return String.valueOf(subtotal);
+	}
+	
+	@PostMapping("/carrito/remove/{pid}")
+	public String removeProductCart(@PathVariable("pid") Integer idProducto, HttpSession session) {
+
+		Usuario cliente = (Usuario) session.getAttribute("usersession");
+
+		if (cliente == null) {
+			return "Necesitas Iniciar Sesion Primero";
+		}
+
+		cardao.removeProductCart(idProducto, cliente);
+		return "El producto ha sido removido del Carrito";
 	}
 }
