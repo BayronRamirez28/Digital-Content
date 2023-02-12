71328aa16e6339f6a9d4be47676e2dbd294b3ac6
Brayan Salguero <107779782+DonBrayaan@users.noreply.github.com>
2023-02-11 19:44:23 -0500

    Update de la implementacion del Carrito
    Metodos para funcionalidad del Carrito

--- src/main/java/com/DigitalContentV2/DigitalContentv2/facadeImp/Cardao.java
+++ src/main/java/com/DigitalContentV2/DigitalContentv2/facadeImp/Cardao.java
@@ -2,6 +2,8 @@ package com.DigitalContentV2.DigitalContentv2.facadeImp;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,24 +14,25 @@ import com.DigitalContentV2.DigitalContentv2.repository.CarRepository;
 import com.DigitalContentV2.DigitalContentv2.repository.ProductoRepository;
 
 @Service
+@Transactional
 public class Cardao {
 
 	@Autowired
 	private CarRepository carRepository;
-	
+
 	@Autowired
 	private ProductoRepository productoRepository;
-	
-	public List<Car_items> listCarItems(Usuario cliente){ 
+
+	public List<Car_items> listCarItems(Usuario cliente) {
 		return carRepository.findByCliente(cliente);
 	}
-	
+
 	public Integer addProduct(Integer idProducto, Integer cantidad, Usuario cliente) {
 		Integer agregarCantidad = cantidad;
-		
+
 		Producto producto = productoRepository.findById(idProducto).get();
 		Car_items car_items = carRepository.findByUsuarioAndProducto(cliente, producto);
-		
+
 		if (car_items != null) {
 			agregarCantidad = car_items.getCantidad() + cantidad;
 			car_items.setCantidad(agregarCantidad);
@@ -39,9 +42,21 @@ public class Cardao {
 			car_items.setCliente(cliente);
 			car_items.setProduct(producto);
 		}
-		
+
 		carRepository.save(car_items);
-		
+
 		return agregarCantidad;
+	}
+
+	public float actualizarCantidad(Integer idProducto, Integer cantidad, Usuario cliente) {
+		System.out.println("updateCantidad " + idProducto + "-" + cantidad + "-" + cliente.getIdUsuario());
+		carRepository.updateCantidad(cantidad, idProducto, cliente.getIdUsuario());
+		Producto producto = productoRepository.findById(idProducto).get();
+		float subtotal = producto.getPrecio() * cantidad;
+		return subtotal;
+	}
+	
+	public void removeProductCart(Integer idProducto, Usuario cliente) {
+		carRepository.deleteByClienteAndProducto(cliente.getIdUsuario(), idProducto);
 	}
 }
